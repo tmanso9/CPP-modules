0d6d90463a7c9b87f266c7b6bd9180cbf2395adc
tmanso9 <tmanso92@gmail.com>
2023-05-25 15:07:55 +0100

    CPP06 ex02 | ex done

--- CPP06/ex02/src/main.cpp
+++ CPP06/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:35:17 by touteiro          #+#    #+#             */
-/*   Updated: 2023/05/24 19:55:00 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/05/25 15:06:47 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,60 @@
 #include "C.hpp"
 #include <cstdlib>
 
+Base	*generate( void );
+void	identify(Base & p);
+void	identify(Base* p);
+
+int	main(void)
+{
+	std::srand(time(NULL));
+	
+	Base *base = generate();
+	
+	std::cout << "ID by pointer:\t\t";
+	identify(base);
+	std::cout << "ID by reference:\t";
+	identify(*base);
+
+	delete base;
+}
+
 Base	*generate( void )
 {
-	int	random = std::rand() % 1000;
-	if (random < 333) {
+	int	random = std::rand() % 3;
+	switch (random)
+	{
+	case 0:
 		return new A;
-	} else if (random < 666) {
+		break;
+	case 1:
 		return new B;
-	} else {
+		break;
+	default:
 		return new C;
+		break;
+	}
+}
+
+void	identify(Base* p)
+{
+	A*	a = dynamic_cast<A*>(p);
+	if (a)
+	{
+		std::cout << "A" << std::endl;
+		return ;
+	}
+	B*	b = dynamic_cast<B*>(p);
+	if (b)
+	{
+		std::cout << "B" << std::endl;
+		return ;
+	}
+	C*	c = dynamic_cast<C*>(p);
+	if (c)
+	{
+		std::cout << "C" << std::endl;
+		return ;
 	}
 }
 
@@ -59,14 +104,4 @@ void	identify(Base & p)
 	catch(const std::exception& e)
 	{
 	}
-}
-
-int	main(void)
-{
-	std::srand(time(NULL));
-	Base *base = generate();
-	std::cout << dynamic_cast<A*>(base) << std::endl;
-	identify(*base);
-
-	delete base;
 }
