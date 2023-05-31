8730d25a88526576734bd7f3b57f5b524a041864
tmanso9 <tmanso92@gmail.com>
2023-05-31 19:36:27 +0100

    CPP09 ex00 | started, boilerplate done

---
+++ CPP09/ex00/incs/BitcoinExchange.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 19:28:47 by touteiro          #+#    #+#             */
+/*   Updated: 2023/05/31 19:32:52 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <map>
+
+class BitcoinExchange
+{
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+
+private:
+	
+};
+
+#endif // !BITCOINEXCHANGE_HPP
---
+++ CPP09/ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 19:32:33 by touteiro          #+#    #+#             */
+/*   Updated: 2023/05/31 19:32:47 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
